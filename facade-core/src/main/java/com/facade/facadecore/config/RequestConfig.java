b32ba13878bb200bdb88d163a5f1398d7d31135e
Zhengliang Wang <zwang684@uottawa.ca>
2024-04-29 13:42:52 -0400

    feat: added redis ip blocker for malicious requests

--- facade-core/src/main/java/com/facade/facadecore/config/RequestConfig.java
+++ facade-core/src/main/java/com/facade/facadecore/config/RequestConfig.java
@@ -1,6 +1,7 @@
 package com.facade.facadecore.config;
 
 import com.facade.facadecore.interceptor.HttpRequestInterceptor;
+import com.facade.facadecore.redis.RedisManager;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -15,14 +16,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class RequestConfig implements WebMvcConfigurer {
 
     private final SecretConfig secretConfig;
+    private final RedisManager redisManager;
 
-    public RequestConfig(SecretConfig secretConfig) {
+    public RequestConfig(SecretConfig secretConfig, RedisManager redisManager) {
         this.secretConfig = secretConfig;
+        this.redisManager = redisManager;
     }
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(new HttpRequestInterceptor());
+        registry.addInterceptor(new HttpRequestInterceptor(redisManager));
 //        registry.addInterceptor(new SecretTokenInterceptor(secretConfig));
     }
 }
--- facade-core/src/main/java/com/facade/facadecore/interceptor/HttpRequestInterceptor.java
+++ facade-core/src/main/java/com/facade/facadecore/interceptor/HttpRequestInterceptor.java
@@ -1,8 +1,10 @@
 package com.facade.facadecore.interceptor;
 
+import com.facade.facadecore.redis.RedisManager;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -16,8 +18,22 @@ import org.springframework.web.servlet.ModelAndView;
 @Component
 @Slf4j
 public class HttpRequestInterceptor implements HandlerInterceptor {
+
+    private final RedisManager redisManager;
+
+    public HttpRequestInterceptor(RedisManager redisManager) {
+        this.redisManager = redisManager;
+    }
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        String remoteAddr = request.getRemoteAddr();
+        Object o = redisManager.hGet("blacklist", remoteAddr);
+        if(ObjectUtils.isNotEmpty(o)) {
+            log.warn("Suspicious IP: {} captured...", remoteAddr);
+            return false;
+        }
+
         // This method is called before the controller method is invoked
         log.debug("Request URL::" + request.getRequestURL().toString() + " Sent to Handler :: " + handler);
         return true;
@@ -28,6 +44,15 @@ public class HttpRequestInterceptor implements HandlerInterceptor {
                            ModelAndView modelAndView) {
         // This method is called after the controller method is invoked, but before the view is rendered
         log.debug("Request URL::" + request.getRequestURL().toString() + " Handler returned :: " + handler);
+
+        int status = response.getStatus();
+
+        if(status > 400) {
+            String remoteAddr = request.getRemoteAddr();
+            redisManager.hSet("blacklist", remoteAddr, "0", 5 * 60);
+            log.warn("Suspicious request with status: {}, save to black list for 5 minutes. IP: {}", status, remoteAddr);
+        }
+
     }
 
     @Override
